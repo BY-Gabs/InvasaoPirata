93bda52607660b4b0ddd910c8e7e56ede138e22a
BY-Gabs <97766069+BY-Gabs@users.noreply.github.com>
2022-06-02 14:50:59 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -50,7 +50,7 @@ function setup() {
   world = engine.world;
   angle = -PI / 4;
   ground = new Ground(0, height - 1, width * 2, 1);
-  tower = new Tower(220, h-280, 200, 280); //alterações para publicação na PlayStore
+  tower = new Tower(220, h-280, 200, 380); //alterações para publicação na PlayStore
   cannon = new Cannon(180, h-495, 130, 100, angle,h,w); //alterações para publicação na PlayStore
 
   var boatFrames = boatSpritedata.frames;
